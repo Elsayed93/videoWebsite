11d5744c22a0308fcc638a96dadd7e3e795faf0b
Elsayed <elsayedelbeshry119@gmail.com>
2020-12-12 02:03:24 +0200

    form enhancement

--- resources/views/back-end/users/form.blade.php
+++ resources/views/back-end/users/form.blade.php
@@ -1,35 +1,44 @@
 @csrf
 <div class="form-group">
+
+    @php $input = 'name'; @endphp
     <label for="exampleInputEmail1">User Name</label>
-    <input type="text" class="form-control @error('name')
-                is-invalid
-    @enderror" value="{{ isset($user) ? $user->name : '' }}" id="exampleInputEmail1" aria-describedby="emailHelp"
-        name="name">
-    @error('name')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-    @enderror
-</div>
-<div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control @error('email')
-                is-invalid
-    @enderror" value="{{ isset($user) ? $user->email : '' }}" id="exampleInputEmail1" aria-describedby="emailHelp"
-        name="email">
-    @error('email')
+    <input type="{{$input}}" class="form-control @error($input)
+                        is-invalid
+    @enderror" value="{{ isset($user) ? $user->{$input} : '' }}" id="exampleInputEmail1" aria-describedby="emailHelp"
+        name="{{ $input }}">
+    @error($input)
         <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
         </span>
     @enderror
 
 </div>
+
 <div class="form-group">
+
+    @php $input = 'email'; @endphp
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="{{ $input }}" class="form-control @error($input)
+                        is-invalid
+    @enderror" value="{{ isset($user) ? $user->{$input} : '' }}" id="exampleInputEmail1" aria-describedby="emailHelp"
+        name="{{ $input }}">
+    @error($input)
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
+
+</div>
+
+<div class="form-group">
+
+    @php $input = 'password'; @endphp
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control @error('password')
-                is-invalid
-    @enderror" id="exampleInputPassword1" name="password">
-    @error('password')
+    <input type="{{$input}}" class="form-control @error($input)
+                        is-invalid
+    @enderror" id="exampleInputPassword1" name="{{$input}}">
+    @error($input)
         <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
         </span>
